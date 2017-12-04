162ca04d143c2a31f41a14826fafe21105e70439
steviegalluscio <galluscio.stevie@gmail.com>
2017-12-04 09:47:35 -0500

    Update Day01.js

--- Day01/Day01.js
+++ Day01/Day01.js
@@ -3,8 +3,8 @@ var input = "2175234281493376693817212167497687911136241765326152235785581689365
 //--- Part One ---
 function inverseCaptcha(sequence){
 	var i = 0,
-		length = sequence.length,
-		result = 0;
+	length = sequence.length,
+	result = 0;
   
 	for (i = 0; i < length; i++){
 		if (sequence.charAt(i) == sequence.charAt(i+1)){
@@ -23,9 +23,9 @@ console.log(inverseCaptcha(input));
 //--- Part Two ---
 function circularInverseCaptcha(sequence){
 	var i = 0,
-  		length = sequence.length,
-  		halfLength = length/2,
-  		result = 0;
+  	length = sequence.length,
+  	halfLength = length/2,
+  	result = 0;
   
   for (i = 0; i < length; i++){
   	if (sequence.charAt(i) == sequence.charAt((i+halfLength)%length)){
