92c3d5db909fabd584930c64a2b0148a8f805ace
Stevie Galluscio <galluscio.stevie@gmail.com>
2024-12-01 13:02:20 -0600

    Check in day 1

--- 2024/Day01/DayOne.java
+++ 2024/Day01/DayOne.java
@@ -2,32 +2,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class DayOne {
     public static void main(String [] args) throws IOException{
         Path filePath = Paths.get("input.txt");
-
-        // Read input into String
-        /* 
-        String input = Files.readString(filePath);
-        System.out.println(input);
-        */
-
-        // Split csv into List
-        /*
-        List<String> items = Arrays.asList(input.split(","));
-        for (String item : items) {
-            System.out.println(item);
-        }
-        */
-
-        // Read input line into List of Strings
+        // Read input lines as List of Strings
         List<String> lines = Files.readAllLines(filePath);
-        for (String line : lines) {
-            System.out.println(line);
+
+        partOne(lines);
+        partTwo(lines);
+    }
+
+    // Worst case time complexity: O(n*log(n))
+    // Because uses min heap to sort with O(log(n)) enqueing and dequeing
+    public static void partOne(List<String> lines) throws IOException{
+        PriorityQueue<Integer> minHeapLeft = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> minHeapRight = new PriorityQueue<Integer>();
+        for(String line: lines){
+            String [] listItems = line.split("   ");
+            int numberLeft = Integer.valueOf(listItems[0]);
+            int numberRight = Integer.valueOf(listItems[1]);
+            minHeapLeft.add(numberLeft);
+            minHeapRight.add(numberRight);
         }
-        
+        long totalDistance = 0;
+        for(int i = 0; i < lines.size(); i++){
+            int d = Math.abs(minHeapLeft.poll() - minHeapRight.poll());
+            totalDistance += d;
+        }
+        System.out.println(totalDistance);
+    }
+
+    // Worst case time complexity: O(n*log(n))
+    // Because Integer implements Comparable, map add/get is O(log(n))
+    public static void partTwo(List<String> lines) throws IOException{
+        List<Integer> listLeft = new ArrayList<>();
+        Map<Integer, Integer> mapRight = new HashMap<>();
+        // Populate map with <numberRight, numberRight's occurrences in right list>
+        for (String line : lines) {
+            String [] listItems = line.split("   ");
+            int numberLeft = Integer.valueOf(listItems[0]);
+            int numberRight = Integer.valueOf(listItems[1]);
+            listLeft.add(numberLeft);
+            mapRight.merge(numberRight, 1, Integer::sum); // Increment occurrences for number by 1
+        }
+        // Loop over left list and lookup occurrences in right
+        long similarityScore = 0;
+        for (Integer leftItem : listLeft) {
+            similarityScore += leftItem * mapRight.getOrDefault(leftItem, 0);
+        }
+        System.out.println(similarityScore);
     }
 }
