714d119e344313d73c7d4c9cf3b75542e0aae128
Stevie Galluscio <galluscio.stevie@gmail.com>
2024-12-02 21:22:27 -0600

    Check in day 2

--- 2024/Day02/DayTwo.java
+++ 2024/Day02/DayTwo.java
@@ -2,48 +2,72 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DayTwo {
     public static void main(String [] args) throws IOException{
-        //Path filePath = Paths.get("input.txt");
-
-        // Read input into String
-        /* 
-        String input = Files.readString(filePath);
-        System.out.println(input);
-        */
-
-        // Split csv into List
-        /*
-        List<String> items = Arrays.asList(input.split(","));
-        for (String item : items) {
-            System.out.println(item);
-        }
-        */
         Path filePath = Paths.get("input.txt");
-        // Read input line into List of Strings
-        List<String> lines = Files.readAllLines(filePath);
+        // Read input lines as List of Strings
+        List<String> reports = Files.readAllLines(filePath);
 
-        partOne(lines);
-        //partTwo(lines);
+        partOne(reports);
+        partTwo(reports);
     }
 
 
-    public static void partOne(List<String> lines) throws IOException{
-        for(String line: lines){
-
+    public static void partOne(List<String> reports) throws IOException{
+        int safeCount = 0;
+        for(String report: reports){
+            String [] levels = report.split(" ");
+            if(checkReport(levels, -1)){ safeCount++; }
         }
-        System.out.println();
+        System.out.println(safeCount);
     }
 
-    public static void partTwo(List<String> lines) throws IOException{      
 
-        for (String line : lines) {
-            
+    // Bruteforce by trying to skip each level until report is safe
+    public static void partTwo(List<String> reports) throws IOException{
+        int safeCount = 0;
+        for(String report: reports){
+            String [] levels = report.split(" ");
+            for(int i = -1; i < levels.length; i++){
+                if(checkReport(levels, i)){
+                    safeCount++;
+                    break;
+                }
+            }
         }
+        System.out.println(safeCount);
+    }
 
-        System.out.println();
+    public static boolean checkReport(String [] levels, int skip){
+        boolean safe = true;
+        boolean inc = true;
+        boolean dec = true;
+        for(int i = 0; i < levels.length - 1; i++){
+            int p1 = i;
+            int p2 = i+1;
+            if(p1 == skip){
+                continue;
+            } else if (p2 == skip){
+                if(p2 >= levels.length - 1){ break; }
+                p2++;
+            }
+            int curLevel = Integer.valueOf(levels[p1]);
+            int nextLevel = Integer.valueOf(levels[p2]);
+            int dist = Math.abs(curLevel - nextLevel);
+            if(!(dist > 0 && dist < 4)){
+                safe = false;
+                break;
+            }
+            if(curLevel < nextLevel){
+                dec = false;
+            }
+            if(nextLevel < curLevel){
+                inc = false;
+            }
+        }
+        if(safe && ((dec && !inc) || (!dec && inc)) ){ return true; }
+        return false;
     }
 }
